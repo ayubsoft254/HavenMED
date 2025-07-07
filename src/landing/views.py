3c953dc7c74264f5bb0fe9f5971860b7de951717
ayubsoft254 <henryoayub15@gmail.com>
2025-07-07 21:28:45 +0300

    Add landing_page view to render the landing page

--- src/landing/views.py
+++ src/landing/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def landing_page(request):
+    """
+    Render the landing page.
+    """
+    return render(request, 'landing/landing_page.html')
